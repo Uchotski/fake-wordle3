965abe115b1ee32abeb2cccd5165cee0d9ed4e04
Uchotski <smith.uchotski@gmail.com>
2022-08-13 20:24:41 +0100

    Changed arrangement of js files as couldn't keep keyboard as a module

--- javascript/modules/coregame.js
+++ javascript/modules/coregame.js
@@ -1,6 +1,6 @@
 import { wordLength } from './constants.js';
-import {  keyboardInput } from './keyboard.js';
 
+//Script to generate word:
 const generateWord = (length) => {
     const wordToGuess = [];
 
@@ -13,8 +13,81 @@ const generateWord = (length) => {
     return wordToGuess;
 }
 
-const startGame = () => {
-    generateWord(wordLength);
+//Script to collect answer:
+const collectAnswer = (row) => {
+    const userGuess = [];
+    row.querySelectorAll("div:not(:first-of-type) > p").forEach(item => userGuess.push(item.innerHTML));
+    return userGuess;
 }
 
-export { generateWord, startGame }
+//Script to process answer:
+const processAnswer = (trueAnswer, userGuess) => {
+    return console.log(trueAnswer, userGuess);
+}
+
+const startGame = () => {
+    //Generate a word:
+    const wordToGuess = generateWord(wordLength);
+    console.log(wordToGuess);
+
+    //Script for keyboard input:
+    let row = 0;
+    let col = 1;
+
+    const keyboardInput = () => {
+        //CONSTANTS:
+        //Row/Tiles to edit:
+        const currentRow = document.getElementById("game-window").getElementsByClassName("row")[row];
+        const tilesInRow = currentRow.getElementsByTagName("div");
+
+        //User Input:
+        let input;
+
+        //FUNCTIONS:
+        //Handling User Input:
+        if (event.type === "click") {
+            input = event.target.innerHTML;
+        } else if (event.type === "keydown") {
+            input = event.key.toLowerCase();
+            if (input.toLowerCase() === "backspace") { input = "delete"; }
+        } else {
+            returnconsole.log("Something went wrong!");
+        }
+
+        //Filter Input to contain only alphabetical characters...
+        if (!/[A-Za-z]/.test(input)) { return; }
+
+        //Adds letters to the input
+        if (input.length === 1 && col <= wordLength) {
+            tilesInRow[col].innerHTML = `<p>${input}</p>`;
+            if (col < wordLength + 1) { col++ };
+        }
+
+        //Removes letters from the input
+        if (input === "delete" && col > 1) {
+            tilesInRow[col - 1].innerHTML = "";
+            col--;
+        }
+
+        //Submits a complete guess
+        if (input === "enter" && col === wordLength + 1) {
+            //Collect the guess as an array...
+            const userGuess = collectAnswer(currentRow);
+
+            //Process the guess...
+            processAnswer(wordToGuess, userGuess);
+
+            //Change row...
+            row++;
+            col = 1;
+        }
+
+        return;
+    }
+
+    //Make the keyboard typeable:
+    document.getElementById("keyboard").addEventListener("click", keyboardInput);
+    document.addEventListener("keydown", keyboardInput);
+}
+
+export { collectAnswer, processAnswer, startGame }
--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,3 @@
-import {  keyboardInput } from './modules/keyboard.js';
 import { startGame } from './modules/coregame.js';
 
-document.addEventListener("keydown", keyboardInput);
-document.getElementById("keyboard").addEventListener("click", keyboardInput);
 document.getElementById("start").addEventListener("click", startGame);
